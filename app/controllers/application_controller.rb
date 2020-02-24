cd16c2d3acadff6401f8c615e51174c25dfacbcb
Micah Levine <micah.levine@outlook.com>
2020-02-24 16:01:02 +0000

    added sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,45 @@ class ApplicationController < Sinatra::Base
   get "/signup" do
     erb :signup
   end
+
+  post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      User.create(username: params[:username], password: params[:password])
+      redirect '/login'
+    end
+
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
+
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/account"
+    else
+      redirect to "/failure"
+    end
+  end
+  
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
   
   
   helpers do
@@ -25,5 +64,7 @@ class ApplicationController < Sinatra::Base
     def logged_in?
       !!session[:user_id]
     end
+    
+  end
 
 end
