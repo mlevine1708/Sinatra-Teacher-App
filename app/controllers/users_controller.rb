882fab950cef88ce1157a28a4f42666843d41841
Micah Levine <micah.levine@outlook.com>
2020-02-26 03:11:46 +0000

    added dynamic route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,15 @@ class UsersController < ApplicationController
   post '/login' do
     user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
-        binding.pry
+      session[:user_id] = user.id 
+      redirect "/users/#{user.id}"
       else
       redirect '/login'
       end 
   end 
   
-  get '/users/show' do
-    erb :'users/show'
+  get '/users/:id' do
+    "Users Show Route"
   end
   
   get '/logout' do
