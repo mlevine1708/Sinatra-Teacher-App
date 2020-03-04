a27aeff5ef43272eeca074e6718be061d3ff3b82
Micah Levine <micah.levine@outlook.com>
2020-03-04 18:43:15 +0000

    added category id

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200304152144) do
+ActiveRecord::Schema.define(version: 20200304184109) do
 
   create_table "assignments", force: :cascade do |t|
     t.string  "title"
     t.string  "subject"
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
